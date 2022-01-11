b62c41515143f12ba443566ffa45f21e2a4fcc14
epalominolan <97476199+epalominolan@users.noreply.github.com>
2022-01-11 16:37:48 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,12 @@
 #---------------------------------------------------
 # AWS SQS queue
 #---------------------------------------------------
-output "values" {
+output "sqs_queue_arn" {
   description = "The URL for the created Amazon SQS queue."
-  value       =  aws_sqs_queue.sqs_queue.values 
-  # element(concat(aws_sqs_queue.sqs_queue.*.arn, [""]), 0)
+  value       = element(concat(aws_sqs_queue.sqs_queue.*.arn, [""]), 0)
 }
 
-#output "sqs_queue_id" {
-#  description = "The ARN of the SQS queue"
-#  value       = element(concat(aws_sqs_queue.sqs_queue.*.id, [""]), 0)
-#}
+output "sqs_queue_id" {
+  description = "The ARN of the SQS queue"
+  value       = element(concat(aws_sqs_queue.sqs_queue.*.id, [""]), 0)
+}
