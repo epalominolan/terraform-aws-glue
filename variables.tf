6f29406bf9198688d1897761544ed0464f53fafa
epalominolan <97476199+epalominolan@users.noreply.github.com>
2022-01-11 16:39:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,12 +23,6 @@ variable "kms_data_key_reuse_period_seconds"{
   default = 300
 }
 
-variable "kms_master_key_id" {
-description = "(Optional) The ID of an AWS-managed customer master key (CMK) for Amazon SQS or a custom CMK"
-type = string
-default  = ""
-
-}
 
 #-----------------------------------------------------------
 # SQS queue
@@ -83,10 +77,6 @@ variable "kms_master_key_id" {
   default     = "alias/aws/sqs"
 }
 
-variable "kms_data_key_reuse_period_seconds" {
-  description = "The length of time, in seconds, for which Amazon SQS can reuse a data key to encrypt or decrypt messages before calling AWS KMS again. An integer representing seconds, between 60 seconds (1 minute) and 86,400 seconds (24 hours). The default is 300 (5 minutes)."
-  default     = 300
-}
 
 variable "maxReceiveCount" {
   description = "Value for redrive_policy (set maxReceiveCount for dead_letter_queue)"
